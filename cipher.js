e5d48fb9436721805faead5d0b27a92d8b2bc936
Sveta K <test@test.test>
2020-10-01 01:14:12 +0100

    feat: add parameters check

--- cipher.js
+++ cipher.js
@@ -1,23 +1,29 @@
 module.exports = function cipher(action, shift, input) {
     const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    if (!action || !shift || !input || isNaN(Number.parseFloat(shift))) {
+        console.error("error: Wrong parameter (action or shift or input) value. Change command and try again.");
+        process.exit(1);
+    };
 
     return input
         .split("").map(symbol => {
+            console.log(input, symbol);
             let capital = (symbol === symbol.toUpperCase())
             let index = ALPHABET.indexOf(symbol.toLowerCase());
-
+            console.log(index, shift);
             if (~index) {
                 if (action === "encode") index += +shift;
+                console.log(index);
                 if (action === "decode") index -= +shift;
-
+                console.log(index);
                 if (index >= 26) index -= 26;
                 if (index < 0) index = 26 - index;
-
+                console.log(index);
                 symbol = ALPHABET[index];
             }
+            console.log(symbol);
             return capital
                 ? symbol.toUpperCase()
                 : symbol;
         }).join("");
-
 }
