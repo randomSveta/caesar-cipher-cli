37ce653bc61bd0e8a3376048aa99800511d611a5
Sveta K <test@test.test>
2020-10-01 01:29:42 +0100

    fix: remove console.log

--- cipher.js
+++ cipher.js
@@ -7,21 +7,20 @@ module.exports = function cipher(action, shift, input) {
 
     return input
         .split("").map(symbol => {
-            console.log(input, symbol);
+
             let capital = (symbol === symbol.toUpperCase())
             let index = ALPHABET.indexOf(symbol.toLowerCase());
-            console.log(index, shift);
+
             if (~index) {
                 if (action === "encode") index += +shift;
-                console.log(index);
                 if (action === "decode") index -= +shift;
-                console.log(index);
+
                 if (index >= 26) index -= 26;
                 if (index < 0) index = 26 - index;
-                console.log(index);
+
                 symbol = ALPHABET[index];
             }
-            console.log(symbol);
+
             return capital
                 ? symbol.toUpperCase()
                 : symbol;
