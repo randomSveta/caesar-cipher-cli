12d563c53a1f0acc77cedff255a13bda455798c2
Sveta K <test@test.test>
2020-10-01 01:20:47 +0100

    feat: add files (from recievedOptions) existance check

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,14 +1,14 @@
 // ./bin/my_caesar_cli.js
 
 const { pipeline } = require('stream');
-const { CaesarCipherTransform } = require('./CaesarCipherTransform')
+const { CaesarCipherTransform } = require('./CaesarCipherTransform');
 const optionsCLI = require('./options');
 const fs = require('fs');
 
 const receivedOptions = optionsCLI();
 
-const readable = fs.createReadStream(receivedOptions.input);
-const writable = fs.createWriteStream(receivedOptions.output);
+const readable = receivedOptions.input ? fs.createReadStream(receivedOptions.input) : process.stdin;
+const writable = receivedOptions.output ? fs.createWriteStream(receivedOptions.output, { flags: "a" }) : process.stdout;
 
 pipeline(
     readable,
